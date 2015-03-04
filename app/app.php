4f40fbb0f3a9ec6fe7cd902272d1c36fc7b1a761
kenna warsinske & Kyle Giard-Chase <student@epicodus.com>
2015-03-04 10:11:21 -0800

    Add  array.

--- app/app.php
+++ app/app.php
@@ -3,6 +3,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
 
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
     $app = new Silex\Application();
 
     $app->get("/", function(){
@@ -13,7 +18,12 @@
         $list_of_tasks = array($test_task, $another_test_task, $third_task);
         $output = "";
 
-        foreach ($list_of_tasks as $task)
+        foreach ($list_of_tasks as $task) {
+            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        }
+
+        return $output;
+
 
     });
 
