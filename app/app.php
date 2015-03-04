d435e2796ab7e8ef79f33f58641d9ad7f24c3a9d
kenna warsinske & Kyle Giard-Chase <student@epicodus.com>
2015-03-04 11:28:58 -0800

    Fix bug in   get.

--- app/app.php
+++ app/app.php
@@ -4,50 +4,52 @@
     require_once __DIR__."/../src/Task.php";
 
     session_start();
+
     if (empty($_SESSION['list_of_tasks'])) {
         $_SESSION['list_of_tasks'] = array();
     }
 
     $app = new Silex\Application();
 
-    $app->get("/", function(){
+    $app->get("/", function() {
 
         $output = "";
 
-        $all_tasks = Task::getAll();
 
-        if(!empty($all_tasks)) {
+        if(!empty(Task::getAll())) {
             $output = $output . "
                 <h1>To Do List</h1>
                 <p>Here are all of your tasks:</p>
                 <ul>";
 
-            foreach ($all_tasks as $task) {
+            foreach (Task::getAll() as $task) {
                 $output = $output . "<p>" . $task->getDescription() . "</p>";
-        }
+            }
+
             $output = $output ."</ul>";
 
-        $output = $output . "</ul>
+        }
+
+        $output = $output . "
             <form action='/tasks' method='post'>
                 <label for='description'>Task Description</label>
                 <input id='description' name='description' type='text'>
 
                 <button type='submit'>Add Task</button>
             </form>
-";
+        ";
         return $output;
 
-        $app->post("/tasks", function() {
-                $task = new Task($_POST['description']);
-                $task->save();
-                return "
-                    <h1>You created a task!</h1>
-                    <p>" . $task->getDescription() . "</p>
-                    <p><a href='/'>View your list of things to do.</a></p>
-                ";
+    });
 
-        });
-
+    $app->post("/tasks", function() {
+            $task = new Task($_POST['description']);
+            $task->save();
+            return "
+                <h1>You created a task!</h1>
+                <p>" . $task->getDescription() . "</p>
+                <p><a href='/'>View your list of things to do.</a></p>
+            ";
 
     });
 
