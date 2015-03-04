6efe0eddfeb344f85749944bcb7949ccda515330
kenna warsinske & Kyle Giard-Chase <student@epicodus.com>
2015-03-04 10:02:19 -0800

    Set route for

--- app/app.php
+++ app/app.php
@@ -5,5 +5,17 @@
 
     $app = new Silex\Application();
 
+    $app->get("/", function(){
+        $test_task = new Task("Learn PHP.");
+        $another_test_task = new Task("Learn Drupal.");
+        $third_task = new Task("Visit France.");
+
+        $list_of_tasks = array($test_task, $another_test_task, $third_task);
+        $output = "";
+
+        foreach ($list_of_tasks as $task)
+
+    });
+
     return $app;
 ?>
