bd301c103e8a042b252392a2c16dd4713ab6981c
kenna warsinske & Kyle Giard-Chase <student@epicodus.com>
2015-03-04 10:54:09 -0800

    create /tasks

--- app/app.php
+++ app/app.php
@@ -28,6 +28,17 @@
 ";
         return $output;
 
+        $app->post("/tasks", function() {
+                $task = new Task($_POST['description']);
+                $task->save();
+                return "
+                    <h1>You created a task!</h1>
+                    <p>" . $task->getDescription() . "</p>
+                    <p><a href='/'>View your list of things to do.</a></p>
+                ";
+
+        });
+
 
     });
 
